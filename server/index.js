d5c43b136d0307d9ada5a1f1e47a15c3ad689196
gamendoza26 <gam70@duke.edu>
2024-10-07 13:40:02 +0200

    Created createDeck function that randomly splits up the deck of cards

--- server/index.js
+++ server/index.js
@@ -11,3 +11,18 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+const createDeck = () => {
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const values = [
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'
+    ];
+    let deck = [];
+
+    suits.forEach(suits => {
+        values.forEach((value, index) => {
+            deck.push({ suit, value, rank: index + 2});
+        });
+    });
+
+    return deck.sort(() => Math.random() - 0.5);
+};
